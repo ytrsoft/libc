e6f0a10b5bbb263d634badaefa6a5a572501fcb1
ytrsoft <1654486219@qq.com>
2025-02-17 02:47:13 +0800

    process

--- libc.h
+++ libc.h
@@ -1,6 +1,36 @@
 #ifndef LIBC_LIBRARY_H
 #define LIBC_LIBRARY_H
 
-float version();
+#include <stdint.h>
+#include <windows.h>
+
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+/**
+ * 进程
+ */
+struct Process {
+    // id
+    DWORD pid;
+    // 父id
+    DWORD ppid;
+    // 位数 32 / 64
+    u32 bits;
+    // 名称
+    char* name;
+    // 路径
+    char* path;
+    // 启动时间
+    u64 st;
+    // 启动图标
+    signed char* si;
+};
+
+signed char* GetProcessIcon(const char* path);
+char* GetProcessPath(DWORD pid);
+u32 GetProcessBits(DWORD pid);
+void FreeProcesses(struct Process* ps, u32 size);
+u32 GetProcesses(struct Process** ps);
 
 #endif
--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include "libc.h"
 
 int main() {
-    printf("version: %f\n", version());
+    struct Process* processes;
+    u32 size = GetProcesses(&processes);
+    for (int i = 0; i < size; ++i) {
+        printf("%s - %d\n", processes[i].name, processes[i].bits);
+    }
+    FreeProcesses(processes, size);
     return 0;
 }
