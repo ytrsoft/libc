ffcb491588ab706f03198dccf97121b073647f9f
ytrsoft <1654486219@qq.com>
2025-02-17 22:20:35 +0800

    update

--- libc.h
+++ libc.h
@@ -8,9 +8,24 @@
 // 写入权限
 #define PROCESS_WRITE_ACCESS (PROCESS_QUERY_INFORMATION | PROCESS_VM_WRITE)
 
+#define BUFFER_SIZE 1024
+
 typedef char* string;
 typedef uint32_t u32;
 typedef unsigned char byte;
+typedef uintptr_t hex;
+
+
+typedef struct tagScan {
+    // 地址
+    hex address;
+    // 当前值
+    int current;
+    // 先前值
+    int previous;
+    // 初始值
+    int first;
+} Scan;
 
 int GetProcessIds(u32** ids);
 string GetProcessName(u32 pid);
